dca34c4c98f7ae08925ef0e67ba985788c349a57
Doldas97(Anle1802) <a.lekedal@gmail.com>
2022-10-25 13:18:02 +0200

    Create CacheConfig.java

---
+++ src/com/hemtest/webserver/configurations/CacheConfig.java
@@ -0,0 +1,21 @@
+package com.hemtest.webserver.configurations;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * Cache Configuration Class
+ *
+ * @author anton lekedal (Doldas)
+ *
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("count"); // caching for /count requests
+    }
+}
