7b2bee6f1744c1e6c4f9a4a59b61ac3980149918
Doldas97(Anle1802) <a.lekedal@gmail.com>
2022-10-31 20:49:07 +0100

    Update WordOccurrencySerivce.java
    * Added new configuration option for word limit.
    * The execute method is now async
    * Re-Formatted the File.

--- src/com/hemtest/webserver/services/WordOccurrencySerivce.java
+++ src/com/hemtest/webserver/services/WordOccurrencySerivce.java
@@ -1,5 +1,9 @@
 package com.hemtest.webserver.services;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.hemtest.webserver.models.WordOccurrence;
@@ -14,20 +18,24 @@ import com.hemtest.webserver.repositories.WordOccurrencyRepository;
 @Service
 public class WordOccurrencySerivce implements IService<String, String> {
 	private final String DELIMITERS = "[ ]";
-	private final Integer LIMIT = 10; // amount of words to be shown
+	@Value("${words.counter.display.max-words:10}")
+	private Integer LIMIT; // amount of words to be shown
 
 	@Override
-	public String execute(String data) {
-		return execute(data,LIMIT);
+	@Async("wordexecutioner")
+	public CompletableFuture<String> execute(String data) {
+		return execute(data, LIMIT);
 	}
-	
+
 	/**
 	 * Executes the service with an additional parameter
-	 * @param data the data to be processed
+	 * 
+	 * @param data  the data to be processed
 	 * @param limit The amount of words to be returned
 	 * @return a sorted list of word occurrences limited by limit
 	 */
-	public String execute(String data,Integer limit) {
+	@Async("wordexecutioner")
+	public CompletableFuture<String> execute(String data, Integer limit) {
 		WordOccurrencyRepository repo = new WordOccurrencyRepository();
 		for (String word : data.split(DELIMITERS)) {
 			if (word.length() > 1)
@@ -36,7 +44,6 @@ public class WordOccurrencySerivce implements IService<String, String> {
 				word = word.substring(0, 1).toUpperCase();
 			repo.create(new WordOccurrence(word));
 		}
-		return repo.toString(repo.getSortedStreamByFrequence(limit, true));
+		return CompletableFuture.completedFuture(repo.toString(repo.getSortedStreamByFrequence(limit, true)));
 	}
-
 }
